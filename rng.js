448045dca92ba49ace5b5cb390d1ded2326296d6
b-boechat <bernardoant@poli.ufrj.br>
2024-03-29 01:17:30 -0300

    RNG changed, number of guesses reduced from 5 to 4, small styling changes.

---
+++ rng.js
@@ -0,0 +1,52 @@
+/*
+function splitmix32(seed) { // splitmix32 -- https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+    return function() {
+        seed |= 0;
+        seed = seed + 0x9e3779b9 | 0;
+        let t = seed ^ seed >>> 16;
+        t = Math.imul(t, 0x21f0aaad);
+        t = t ^ t >>> 15;
+        t = Math.imul(t, 0x735a2d97);
+        return ((t = t ^ t >>> 15) >>> 0) / 4294967296;
+    }
+}
+*/
+
+function cyrb128(seed) {
+    seed = seed.toString()
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < seed.length; i++) {
+        k = seed.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1>>>0, h2>>>0, h3>>>0, h4>>>0];
+}
+
+function sfc32(a, b, c, d) {
+    return function() {
+      a |= 0; b |= 0; c |= 0; d |= 0;
+      let t = (a + b | 0) + d | 0;
+      d = d + 1 | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+  }
+
+
+function get_rng(seed) {
+    proc_seed = cyrb128(seed)
+    rng = sfc32(proc_seed[0], proc_seed[1], proc_seed[2], proc_seed[3])
+    return rng
+}
